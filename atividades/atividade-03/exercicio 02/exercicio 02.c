247de68315118537420bdbaf03152aa91f39c157
vivianeslv <viviane.silva.nascimento08@aluno.ifce.edu.br>
2024-08-07 19:33:47 -0300

    Update exercicio 02.c

--- atividades/atividade-03/exercicio 02/exercicio 02.c
+++ atividades/atividade-03/exercicio 02/exercicio 02.c
@@ -1,1 +1,31 @@
+Qual dos seguintes tipos de variáveis em C pode armazenar números inteiros?
+
+(c) int
+Qual é a sintaxe correta para declarar uma variável chamada idade do tipo int?
+
+(a) int idade;
+Qual operador é usado para atribuir um valor a uma variável em C?
+
+(a) =
+Qual dos seguintes operadores realiza uma multiplicação em C?
+
+(a) *
+O que o operador % faz em C?
+
+(a) Calcula o resto da divisão
+Qual operador é usado para comparar se dois valores são iguais em C?
+
+(b) ==
+Qual operador lógico é usado para verificar se pelo menos uma das condições é verdadeira?
+
+(b) ||
+Qual operador é usado para inverter o valor de uma expressão lógica em C?
+
+(c) !
+Qual operador é usado para deslocar bits para a esquerda em C?
+
+(a) <<
+Qual operador é usado para deslocar bits para a direita em C?
+
+(b) >>
 // Conteúdo do arquivo 
